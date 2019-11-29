0a8b7944ea938178c988cd439b6887c076213b1a
Emerson Costa <costtasemerson@gmail.com>
2019-11-29 09:41:33 -0300

    Finalizando v1

--- main.py
+++ main.py
@@ -9,13 +9,11 @@ def get_valor_registrador(operando):
 	elif operando[1] ==	't':	
 		return registradores.getT(int(operando[2]));
 
-def set_valor_registrador(operando):
+def set_valor_registrador(operando, valor):
 	if operando[1] == 's':
-		return registradores.getS(int(operando[2]));
+		return registradores.setS(int(operando[2]), valor);
 	elif operando[1] ==	't':	
-		return registradores.getT(int(operando[2]));
-
-
+		return registradores.setT(int(operando[2]), valor);
 
 with open(filepath) as fp:
 	line = fp.readline()
@@ -31,17 +29,16 @@ with open(filepath) as fp:
 
 		elif linha[0] == 'ADD':
 			destino = list(linha[1])
-
-			primeiro_operando_valor = get_valor_registrador(list(linha[2]))
-			segundo_operando_valor = get_valor_registrador(list(linha[3]))
-
-
+			primeiro_operando_valor = int(get_valor_registrador(list(linha[2])))
+			segundo_operando_valor = int(get_valor_registrador(list(linha[3])))
+			set_valor_registrador(destino, primeiro_operando_valor + segundo_operando_valor)
 			
 		elif linha[0] == 'SUB':
 			destino = list(linha[1])
-			primeiro_operando = list(linha[2])
-			segundo_operando = list(linha[3])
-			
+			primeiro_operando_valor = int(get_valor_registrador(list(linha[2])))
+			segundo_operando_valor = int(get_valor_registrador(list(linha[3])))
+			set_valor_registrador(destino, primeiro_operando_valor - segundo_operando_valor)
+
 		else:
 			print('hello')
 
